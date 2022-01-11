54963f8b29b3d8b4df8f7c8e8f4b78974233a5a7
Sufiyan Salim Akbani <96812346+sufi0702@users.noreply.github.com>
2022-01-11 11:18:47 +0530

    Update script.js

--- String and its methods/script.js
+++ String and its methods/script.js
@@ -5,18 +5,13 @@ function stringMethods() {
   var lastName = document.getElementById("lname").value;
   var emailID = document.getElementById("email").value;
   var age = parseInt(document.getElementById("age").value);
-  var personType;
+  var personType = age < 11 ? "Kid" : "Genuins";
   var fullName =
     firstName.charAt(0).toUpperCase() +
     firstName.slice(1) +
     " " +
     lastName.charAt(0).toUpperCase() +
     lastName.slice(1);
-  if (age <= 10) {
-    personType = "Kid";
-  } else {
-    personType = "Genuins"
-  }
   document.querySelector("table").innerHTML =
     "<tr><th>Full Name</th><th>Email ID</th><th>Age</th><th>Person Type</th></tr><tr><td>" +
           fullName+"</td><td>"+emailID+"</td><td>"+age+"</td><td>" + personType +"</td></tr>";
